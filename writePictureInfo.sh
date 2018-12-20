1e92199675f560084512420092f26d492dccfe83
lukask8 <28091459+lukask8@users.noreply.github.com>
2018-12-20 16:49:17 +0100

    First beta release

---
+++ writePictureInfo.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+# Print Film mode from Fujifim JPG
+# Usage :
+# $ ./getPictureInfo.sh *.JPG
+#
+# Thanks to "exiftool -v2 option" you get useful info from tags:
+#
+# - CustomRendered = 1 - Tag 0xa401 (2 bytes, int16u[1])
+# - Saturation = 771 - Tag 0x1003 (2 bytes, int16u[1])
+#
+# CustomRendered = 1 + Saturation = 768 = 0x300 => B&W
+# CustomRendered = 1 + Saturation = 770 = 0x301 => B&W Y
+# CustomRendered = 1 + Saturation = 771 = 0x303 => B&W G
+#
+# FilmMode = 0 - Tag 0x1401 (2 bytes, int16u[1])
+#
+# 0 => Provia/Standard
+# 512 = 0x200 => Velvia/Vivid
+# 288 = 0x120 => Astia/Soft
+# 1536 = 0x600 => Classic Chrome
+# 1281 = 0x501=> Neg Hi
+# 1280 = 0x500 => Neg Low
+
+
+if [ -z "$1" ]
+  then
+    echo ""
+    echo "	Print Film mode from Fujifim JPG file"
+    echo ""
+    echo "	Usage:"
+    echo "	$ ./getPictureInfo.sh *.JPG"
+    echo ""
+    exit 0
+fi
+
+COUNTER=0
+PROCESSED=0
+
+######################################################
+for i in "$@"
+do
+		output=`exiftool -v2 "$i" | grep  "CustomRendered = 1"`
+		if   [  -n "$output"  ] ; then
+				# B&W
+				output=`exiftool -v2 "$i" | grep  "Saturation ="`
+				if  [[ "$output" == *"= 768"* ]] ; then
+					keys="B&W"
+					exiftool -XMP:Subject-=$keys -IPTC:keywords-=$keys -XMP:Subject+=$keys  -IPTC:keywords+=$keys "$i"
+					echo "$i" - "B&W"
+    			    PROCESSED=$((PROCESSED+1))
+				fi
+				if  [[ "$output" == *"= 770"* ]] ; then
+					keys="B&W-Y"
+					exiftool -XMP:Subject-=$keys -IPTC:keywords-=$keys -XMP:Subject+=$keys  -IPTC:keywords+=$keys "$i"
+					echo "$i" - "B&W Y"
+    			    PROCESSED=$((PROCESSED+1))
+				fi
+				if  [[ "$output" == *"= 771"* ]] ; then
+					keys="B&W-G"
+					exiftool -XMP:Subject-=$keys -IPTC:keywords-=$keys -XMP:Subject+=$keys  -IPTC:keywords+=$keys "$i"
+					echo "$i" - "B&W G"
+    			    PROCESSED=$((PROCESSED+1))
+				fi				
+		else 
+				# Color film
+				output=`exiftool -v2 "$i" | grep  "FilmMode = "`
+				if [[ "$output" == *"= 0"* ]]; then
+					keys="Provia"
+					exiftool -XMP:Subject-=$keys -IPTC:keywords-=$keys -XMP:Subject+=$keys  -IPTC:keywords+=$keys "$i"				
+				  	echo "$i" - "Provia/Standard"
+				    PROCESSED=$((PROCESSED+1))				  	
+				else 
+					if [[ "$output" == *"= 512"* ]]; then
+						keys="Velvia"
+						exiftool -XMP:Subject-=$keys -IPTC:keywords-=$keys -XMP:Subject+=$keys  -IPTC:keywords+=$keys "$i"							
+				  		echo "$i" - "Velvia/Vivid"	
+					    PROCESSED=$((PROCESSED+1))
+				  	else
+						if [[ "$output" == *"= 288"* ]]; then
+							keys="Astia"
+							exiftool -XMP:Subject-=$keys -IPTC:keywords-=$keys -XMP:Subject+=$keys  -IPTC:keywords+=$keys "$i"										
+				  			echo "$i" - "Astia/Soft"
+						    PROCESSED=$((PROCESSED+1))				  			
+				  		else
+				  			if [[ "$output" == *"= 1536"* ]]; then
+								keys="ClassicChrome"
+								exiftool -XMP:Subject-=$keys -IPTC:keywords-=$keys -XMP:Subject+=$keys  -IPTC:keywords+=$keys "$i"						  			
+				  				echo "$i" - "Classic Chrome"
+  							    PROCESSED=$((PROCESSED+1))				  				
+				  			else
+				  				if [[ "$output" == *"= 1281"* ]]; then
+									keys="NegHi"
+									exiftool -XMP:Subject-=$keys -IPTC:keywords-=$keys -XMP:Subject+=$keys  -IPTC:keywords+=$keys "$i"			
+				  					echo "$i" - "Neg Hi"
+   	  							    PROCESSED=$((PROCESSED+1))
+				  				else
+				  					if [[ "$output" == *"= 1280"* ]]; then
+										keys="NegLow"
+										exiftool -XMP:Subject-=$keys -IPTC:keywords-=$keys -XMP:Subject+=$keys  -IPTC:keywords+=$keys "$i"							  					
+				  						echo "$i" - "Neg Low"
+    	  							    PROCESSED=$((PROCESSED+1))
+				  					fi
+				  				fi
+							fi
+						fi
+				  	fi			
+				fi
+		fi
+
+        COUNTER=$((COUNTER+1))
+done
+echo "Done!"
+######################################################
+echo ""
+echo "Total worked :" $COUNTER
+echo "Total processed :" $PROCESSED
